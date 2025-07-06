9aed3d9d9fd6c3aaff819e600c965ecb9efa0b67
palkin sirohi <palkinsirohi2011@gmail.com>
2025-07-06 17:40:36 +0530

    utiles

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ import dotenv from "dotenv";
  })
 
  connectDB()
+ .then(()=>{
+   app.listen(process.env.PORT || 8000,()=>{
+      console.log(`server is running on port :&{process.env.PORT}`);
+   })
+ })
+ .catch((err)=> {
+   console.log("connection failed",err);
+ })
