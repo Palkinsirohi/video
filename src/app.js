27846393850e5b4d91a52055262cdbc03787cc0b
palkin sirohi <palkinsirohi2011@gmail.com>
2025-07-08 16:00:26 +0530

    multer

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import connectDB from 'cookie-parser';
 import cookieParser from 'cookie-parser';
 
 const app = express()
 app.use(cors({
-    origin:process.enV.CORS_ORIGIN,
+    origin:process.env.CORS_ORIGIN,
     credentials:true
 }
 ))
@@ -13,6 +12,16 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+//routes
 
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users",userRouter)
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal Server Error', error: err.message });
+});
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
- dotenv.config({
+import { app } from "./app.js";
+
+dotenv.config({
     path: './env'
+})
 
- })
-
- connectDB()
- .then(()=>{
-   app.listen(process.env.PORT || 8000,()=>{
-      console.log(`server is running on port :&{process.env.PORT}`);
-   })
- })
- .catch((err)=> {
-   console.log("connection failed",err);
- })
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running on port: ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err) => {
+    console.log("connection failed", err);
+})
